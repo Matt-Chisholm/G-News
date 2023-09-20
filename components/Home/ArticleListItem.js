18bd5191a73e5a74c5424259245741cd663cf78d
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-09-20 11:53:26 -0700

    starter home components

---
+++ components/Home/ArticleListItem.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ArticleListItem = () => {
+  return (
+    <View>
+      <Text>ArticleListItem</Text>
+    </View>
+  );
+};
+
+export default ArticleListItem;
+
+const styles = StyleSheet.create({});
